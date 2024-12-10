d73da4dc77491eae06e4378ac2e0b942f1e4bd47
menna544 <mennamaged121@gmail.com>
2024-12-10 20:51:39 +0200

    commit first section of Home

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-//import './Home.css'; 
+import './home.css'; 
 import Navbar from './Components/Navbar';
 function Home() {
   return (
     <>
     <Navbar />
-    <section id="home">
+    <section className="home">
       <div className="content">
-        <h3>Best time for<br />coffee is now☕️☕️</h3>
+       <div className='inside'>     
+        <h1 className='pp'>Best time for<br />coffee is now☕️☕️</h1>
         <p>Coffee is flavoursome, fragrant and deeply rich in taste (using freshly roasted coffee beans).</p>
-        <button id="btn" onClick={() => alert("Get yours now!")}>Get yours now</button>
+        <button className="btn" onClick={() => alert("Get yours now!")}>get yours now</button>
       </div>
+      </div>  
     </section>
     </> );
 }
