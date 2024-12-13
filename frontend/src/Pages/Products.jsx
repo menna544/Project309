07e27bfdd81a1900c468a70c42c26aadeaf637a6
menna544 <mennamaged121@gmail.com>
2024-12-13 20:04:01 +0200

    adding api for products af coffe

---
+++ frontend/src/Pages/Products.jsx
@@ -0,0 +1,77 @@
+import  Pro1 from '../images/pro1.png'
+import Pro2 from '../images/bg-preview.png'
+import Pro3 from '../images/tte-removebg-preview.png'
+import Pro4 from '../images/pro3-removebg-preview.png'
+import Pro5 from '../images/pro4-removebg-preview.png'
+import Pro6 from '../images/67728-removebg-preview.png'
+import Pro7 from '../images/iced-coff-preview.png'
+import Pro8 from '../images/images.jpg'
+import Pro9 from '../images/ab77947fec930ab10a4d0e58afb54e4d.jpg'
+const products = [
+  {
+    id: 1,
+    name: 'Latte',
+    image: Pro1,
+    price: 35
+  },
+  {
+    id: 2,
+    name: 'Caramel Ice Coffee',
+    image: Pro2,
+    price: 10
+  },
+  {
+    id: 3,
+    name: 'Milk or Milk Substitute',
+    image: Pro3,
+    price: 15
+  },
+  {
+    id: 4,
+    name: 'Espresso',
+    image: Pro4,
+    price: 10
+  },
+  {
+    id: 5,
+    name: 'Green Coffee',
+    image: Pro5,
+    price: 30
+  },
+  {
+    id: 6,
+    name: 'Hot Chocolate',
+    image: Pro6,
+    price: 15
+  },
+  {
+    id: 7,
+    name: 'Oleato™ Caffé Latte with Oatmilk',
+    image: Pro7,
+    price: 10
+  },
+  {
+    id: 8,
+    name: 'Espresso Costa Coffee',
+    image: Pro8,
+    price: 30
+  },
+  {
+    id: 9,
+    name: 'Flat White',
+    image: Pro9,
+    price: 30
+  }
+];
+
+function Products() {
+
+
+
+  return (
+   <>
+   </>
+  );
+}
+
+export default Products;
